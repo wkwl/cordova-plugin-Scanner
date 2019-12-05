5cd1ae4d6bb053b06bbf53bfc1b50abe14838571
wukaiwen <1877508948@qq.com>
2019-12-05 17:04:36 +0800

    no message

--- src/ios/Scanner.m
+++ src/ios/Scanner.m
@@ -6,28 +6,19 @@
 
 - (void)coolMethod:(CDVInvokedUrlCommand*)command
 {
-    QRCodeController *vc = [[QRCodeController alloc] init];
-    vc.ScanBlock = ^(NSString *result){
-        CDVPluginResult *pluginResult = nil;
-        if (result!=nil) {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
-        }else{
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"扫码失败"];
-        }
-
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    };
-    [self.viewController presentViewController:vc animated:YES completion:nil];
-//    CDVPluginResult* pluginResult = nil;
-//    NSString* echo = [command.arguments objectAtIndex:0];
-//
-//    if (echo != nil && [echo length] > 0) {
-//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-//    } else {
-//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-//    }
-//
-//    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        QRCodeController *vc = [[QRCodeController alloc] init];
+        vc.ScanBlock = ^(NSString *result){
+            CDVPluginResult *pluginResult = nil;
+            if (result!=nil) {
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+            }else{
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"扫码失败"];
+            }
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        };
+        [self.viewController presentViewController:vc animated:YES completion:nil];
+    });
 }
 
 @end
